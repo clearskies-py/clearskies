e09112ce0cc85c7a52f8021dfbfd7b9672a523a8
Conor Mancone <cmancone@gmail.com>
2026-07-14 11:45:32 -0400

    feat: add tests

--- src/clearskies/authentication/oauth.py
+++ src/clearskies/authentication/oauth.py
@@ -1,8 +1,15 @@
-from typing import Any
+from __future__ import annotations
 
-from clearskies import Secrets, configs, decorators, di
+import datetime
+import json
+from typing import TYPE_CHECKING, Any
+
+from clearskies import configs, decorators, di
 
 from .jwks import Jwks
+
+if TYPE_CHECKING:
+    from clearskies.secrets import Secrets
 
 
 class Oauth(Jwks, di.InjectableProperties):
@@ -107,7 +114,7 @@ class Oauth(Jwks, di.InjectableProperties):
     environment variables are controlled by the `env_client_key_name` and `env_client_secret_name` configuration settings.
     These default to `OAUTH_CLIENT_ID` and `OAUTH_CLIENT_SECRET`, respectively.
     """
-    client_credentials_in_secret_manager = configs.Boolen(default=True)
+    client_credentials_in_secret_manager = configs.Boolean(default=True)
 
     """
     The secret manager to use.
@@ -133,17 +140,17 @@ class Oauth(Jwks, di.InjectableProperties):
         id = clearskies.columns.String()
     ```
     """
-    secret_manager = configs.Secrets()
+    secret_manager = configs.Secrets(default=None)
 
     """
     The name of the environment key to fetch the client id from (only relevant for client_credentials_in_secret_manager=False).
     """
-    env_client_id_name = configs.string(default="OAUTH_CLIENT_ID")
+    env_client_id_name = configs.String(default="OAUTH_CLIENT_ID")
 
     """
     The name of the environment key to fetch the client secret from (only relevant for client_credentials_in_secret_manager=False)
     """
-    env_client_secret_name = configs.string(default="OAUTH_CLIENT_SECRET")
+    env_client_secret_name = configs.String(default="OAUTH_CLIENT_SECRET")
 
     """
     The path/name of the secret in your secret manager that contains the client credentials.
@@ -152,7 +159,7 @@ class Oauth(Jwks, di.InjectableProperties):
     By default, the keys for these (in the JSON object) should be `client_id` and `client_secret`, but those names
     are controlled via `secret_manager_client_id_key` and `secret_manager_client_secret_key`.
     """
-    secret_manager_secret_name = configs.string(default="")
+    secret_manager_secret_name = configs.String(default="")
 
     """
     The json key inside the secret where the client id is found (default: client_id)
@@ -172,7 +179,7 @@ class Oauth(Jwks, di.InjectableProperties):
     """
     The name of the key in the authentication response where the JWT is found
     """
-    expiration_key = configs.String(default="access_token")
+    token_key = configs.String(default="access_token")
 
     """
     The name of the key in the authentication response where the JWT TTL is found (in seconds).
@@ -183,6 +190,9 @@ class Oauth(Jwks, di.InjectableProperties):
     The JWKS url to use to verify incoming JWTs
     """
     jwks_url = configs.String(required=False)
+
+    secrets = di.inject.Secrets()
+    di = di.inject.Di()
 
     _jwt: str
 
@@ -221,16 +231,33 @@ class Oauth(Jwks, di.InjectableProperties):
             )
 
     def clear_credential_cache(self):
-        self._jwt = None
+        delattr(self, "_jwt")
 
-    def jwt(self):
-        if hasattr(self, "_jwt") or not self._jwt or self.jwt_expiration - self.utcnow > self.jwt_refresh_ttl_sec:
+    @property
+    def jwt(self) -> str:
+        if hasattr(self, "_jwt") and self.expiration - self.now > self.jwt_refresh_ttl_sec:
             return self._jwt
 
         if self.client_credentials_in_secret_manager:
             (client_id, client_secret) = self.fetch_client_credentials_from_secret_manager()
         else:
             (client_id, client_secret) = self.fetch_client_credentials_from_environment()
+
+        more_headers = {}
+        if self.additional_authentication_request_headers:
+            if callable(self.additional_authentication_request_headers):
+                more_headers = self.di.call_function(self.additional_authentication_request_headers)
+                if not isinstance(more_headers, dict):
+                    raise ValueError(
+                        f"I called the callable attached to '{self.__class__.__name__}.additional_authentication_request_headers' but it returned an object of type '{more_headers.__class__.__name__}'.  It must return a dict[str, str]."
+                    )
+                for key, value in more_headers.items():
+                    if not isinstance(key, str) or not isinstance(key, str):
+                        raise ValueError(
+                            f"I called the callable attached to '{self.__class__.__name__}.additional_authentication_request_headers' which must return a dict[str, str].  It returned a dictionary with non-string keys/values."
+                        )
+            else:
+                more_headers = self.additional_authentication_request_headers
 
         response = self.requests.post(
             self.authentication_url,
@@ -240,7 +267,7 @@ class Oauth(Jwks, di.InjectableProperties):
                     "client_id": client_id,
                     "client_secret": client_secret,
                 },
-                **self.additional_authentication_request_headers,
+                **more_headers,
             },
         )
 
@@ -262,12 +289,86 @@ class Oauth(Jwks, di.InjectableProperties):
                 f"Failed to extract JWT expiration from authentication server response.  I am looking for a key named '{self.expiration_key}' but found keys named {keys}"
             )
 
-        self.jwt = response_data[self.token_key]
-        self.expiration = self.now + response_data[self.expiration_key]
-        return self.jwt
+        self._jwt = response_data[self.token_key]
+        self.expiration = self.now + datetime.timedelta(seconds=response_data[self.expiration_key])
+        return self._jwt
+
+    def fetch_client_credentials_from_environment(self) -> tuple[str, str]:
+        """
+        Fetch client credentials from the environment.
+
+        This is called when self.client_credentials_in_secret_manager is set to `False`.  It pulls the client id and client secret
+        from the environment in variables named `self.env_client_id_name` and `self.env_client_id_name`.  Since these come from
+        the environment, automatic secret rotation is not supported.
+        """
+        client_id = self.environment.get(self.env_client_id_name, silent=True)
+        if not client_id:
+            raise ValueError(
+                f"Failed to fetch client id from environment.  Environment variable '{self.env_client_id_name}' was empty."
+            )
+        client_secret = self.environment.get(self.env_client_secret_name, silent=True)
+        if not client_secret:
+            raise ValueError(
+                f"Failed to fetch client secret from environment.  Environment variable '{self.env_client_secret_name}' was empty."
+            )
+
+        return (client_id, client_secret)
+
+    def fetch_client_credentials_from_secret_manager(self) -> tuple[str, str]:
+        """
+        Fetch client credentials from the secret manager.
+
+        The client credentials are not cached at all, but are fetched anew everytime a new JWT is required.
+        As a result, automatic credential rotation is supported out of the box: if the client credentials are rotated
+        in the secret manager, then the new credentials will automatically be used the next time the JWT expires.
+
+        This assumes that there is a single secret that contains both the client id and the client secret as a multi-valued/json
+        secret.  The path to the secret itself must be set in `self.secret_manager_secret_name` and then you must provide the
+        name of the keys where the client id and client secret are stored (via `self.secret_manager_client_id_key`and
+        `self.secret_manager_client_secret_key`).
+        """
+        if self.secret_manager:
+            secret = self.secret_manager.get(self.secret_manager_secret_name, silent_if_not_found=True)
+            if not secret:
+                raise ValueError(
+                    f"I fetched the secret named '{self.secret_manager_secret_name}' from the secret manager provided of type '{self.secret_manager.__class__.__name__}' but the secret was empty."
+                )
+        else:
+            secret = self.secrets.get(self.secret_manager_secret_name, silent_if_not_found=True)
+            if not secret:
+                raise ValueError(
+                    f"I fetched the secret named '{self.secret_manager_secret_name}' from the default secret manager of type '{self.secret_manager.__class__.__name__}' but the secret was empty."
+                )
+
+        if isinstance(secret, str):
+            try:
+                secret = json.loads(secret)
+            except json.JSONDecodeError as e:
+                raise ValueError(
+                    f"I fetched the secret named '{self.secret_manager_secret_name}' successfully but it did not contain a multi-valued/json secret.  I received a string which was not valid JSON."
+                )
+
+        if not isinstance(secret, dict):
+            raise ValueError(
+                f"I fetched the secret named '{self.secret_manager_secret_name}' successfully but it was an object of type '{secret.__class__.__name__}', while I reqiure a dict."
+            )
+        for required_key in [self.secret_manager_client_id_key, self.secret_manager_client_secret_key]:
+            if required_key not in secret:
+                raise KeyError(
+                    f"I successfully fetched the secret named '{self.secret_manager_secret_name}' but it did not contain a key called '{required_key}'"
+                )
+            if not secret.get(required_key):
+                raise KeyError(
+                    f"I successfully fetched the secret named '{self.secret_manager_secret_name}' but the key '{required_key}' was empty"
+                )
+            if not isinstance(secret.get(required_key), str):
+                raise KeyError(
+                    f"I successfully fetched the secret named '{self.secret_manager_secret_name}' but the key '{required_key}' did not contain a string"
+                )
+
+        return (secret[self.secret_manager_client_id_key], secret[self.secret_manager_client_secret_key])
 
     def headers(self, retry_auth=False):
-        self._configured_guard()
         if retry_auth:
             self.clear_credential_cache()
         return {"Authorization": f"Bearer {self.jwt}"}
